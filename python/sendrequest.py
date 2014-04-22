f0088ebd56960eb7c648f2bab12a256d4c8174a0
pranksttee <xhxpecer@gmail.com>
2014-04-22 23:09:00 +0800

    modify-optimize

--- python/sendrequest.py
+++ python/sendrequest.py
@@ -1,13 +1,5 @@
-import urllib
-import json
+import sys, urllib, json
 
-'''
-key_words = ['%E6%95%99%E7%88%B6', 
-	'%E5%A4%A9%E5%A0%82%E7%94%B5%E5%BD%B1%E9%99%A2',
-	'%E7%BE%8E%E5%9B%BD%E5%BE%80%E4%BA%8B',
-	'%E9%A3%98',
-	'%E7%BE%8E%E4%B8%BD%E4%BA%BA%E7%94%9F'];
-'''
 key_words = [];
 f = open('movie.txt');
 line = f.readline();
@@ -17,13 +9,23 @@ while line:
 
 
 result = {};
+i = 1;
 for key in key_words:
-	f = urllib.urlopen('https://api.douban.com/v2/movie/search?count=20&q=' + key);
+	f = urllib.urlopen('https://api.douban.com/v2/movie/search?count=20&q=' + urllib.quote(key));
 
 	obj = f.read();
-	print obj;
 	oobj = json.loads(obj);
-	result[oobj['subjects'][0]['title']] = oobj['subjects'][0]['rating']['average'];
+	if 'subjects' not in oobj:
+		print obj;
+		continue;
+	if len(oobj['subjects']) == 0:
+		print 'not found:', urllib.quote(key), '|index:', i;
+		print '###################################';
+		print oobj;
+		print '###################################';
+	else:
+		result[oobj['subjects'][0]['title']] = oobj['subjects'][0]['rating']['average'];
+	i += 1;
 
 sort_list = sorted(result.iteritems(), key = lambda d:d[1], reverse = True);
 
